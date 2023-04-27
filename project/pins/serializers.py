6fb6dc1480d9c500c41f34cf00c452053b485735
Taehyun <taehyunjo97@gmail.com>
2023-04-28 02:42:22 +0900

    fixed pin serializer

--- project/pins/serializers.py
+++ project/pins/serializers.py
@@ -1,9 +1,7 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
 from musics.models import Music
 from musics.serializers import MusicSerializer
 from pins.models import Pin
-from users.serializers import UserSerializer
 
 class PinSerializer(serializers.ModelSerializer):
     music = MusicSerializer()
@@ -17,11 +15,14 @@ class PinSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         music_data = validated_data.pop('music')
-        music = self.__get_or_create_music(music_data)
+        music = self.__update_or_create_music(music_data)
         user = self.context['request'].user
         pin = Pin.objects.create(music=music, user=user, **validated_data)
         return pin
 
-    def __get_or_create_music(self, music_data):
-        music, created = Music.objects.get_or_create(applemusic_id=music_data['applemusic_id'])
+    def __update_or_create_music(self, music_data):
+        music, created = Music.objects.update_or_create(
+            applemusic_id=music_data['applemusic_id'],
+            defaults=music_data
+        )
         return music
