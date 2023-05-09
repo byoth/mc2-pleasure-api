f4d8f07009a0cc33d014badabe98498f913ad90b
Taehyun <taehyunjo97@gmail.com>
2023-05-09 21:17:14 +0900

    added dummy pins

---
+++ pins/management/commands/create_dummy_pins.py
@@ -0,0 +1,34 @@
+from decimal import Decimal
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+import random
+from musics.models import Music
+from pins.models import Pin
+
+class Command(BaseCommand):
+    help = 'Creates dummy pins.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('pins_count', type=int)
+
+    def handle(self, *args, **options):
+        pins_count = options['pins_count']
+        musics = Music.objects.all()
+        users = User.objects.all()
+
+        for _ in range(pins_count):
+            music = random.choice(musics)
+            user = random.choice(users)
+            pin = Pin.objects.create(
+                music=music,
+                user=user,
+                latitude=Decimal(random.uniform(-90, 90)),
+                longitude=Decimal(random.uniform(-180, 180)),
+                administrative_area='Somewhere',
+                locality='어딘가',
+                weather=random.choice(['sunny', 'cloudy', 'rainy', 'snowy']),
+                temperature=random.randint(-20, 40),
+                created_at=timezone.now(),
+            )
+            print(f"Created dummy Pin: {pin}")
