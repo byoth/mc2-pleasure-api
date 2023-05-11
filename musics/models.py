0b85dab3be9a2a02604bc10f04831fd4ff7626aa
Taehyun <taehyunjo97@gmail.com>
2023-05-11 17:59:47 +0900

    migrated musics

--- musics/models.py
+++ musics/models.py
@@ -4,6 +4,8 @@ class Music(models.Model):
     applemusic_id = models.CharField(max_length=20)
     title = models.CharField(max_length=100)
     artist_name = models.CharField(max_length=100)
+    artwork_url = models.CharField(max_length=200)
+    applemusic_url = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
